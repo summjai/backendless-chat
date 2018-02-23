6dcdac171ee84efa58a41b3b35a081251a1c56cb
summjai <summjai@gmail.com>
2018-02-23 18:06:40 +0300

    removed ignored files, appdelegate print states

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -13,34 +13,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    let stateForm = "Application moved from %@ to %@:"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let _window = window {
+            _window.rootViewController = ViewController()
+            _window.makeKeyAndVisible()
+
+            print(String(format: stateForm, "Not running", "Background"), #function, "\n")
+        
+        // print("Application moved from Not running to Background: \(#function)" )
+            
+        }
+        
         return true
     }
-
+   
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+         print(String(format: stateForm, "Active", "Background"), #function, "\n")
+        
+        // print("Application moved from Active to Background:",#function)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+         print(String(format: stateForm, "Active", "Background"), #function, "\n")
+        
+        // print("Application moved from Active to Background:",#function)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+         print(String(format: stateForm, "Background", "Active"), #function, "\n")
+        
+         //print("Application moved from Background to Active:",#function)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+         print(String(format: stateForm, "Background", "Active"), #function, "\n")
+        
+        //print("Application moved from Background to Active:",#function)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+         print(String(format: stateForm, "Background", "Not running"), #function, "\n")
+        
+        //print("Application moved from Background to Not running:",#function)
     }
+    
 
-
+    func willFinishLaunchingWithOptions(_ application: UIApplication) {
+        
+         print(String(format: stateForm, "Background", "Not running"), #function, "\n")
+        
+        //print("Application moved from Background to Not running:",#function)
+    }
 }
 
--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+    
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view, typically from a nib.
     }
 
